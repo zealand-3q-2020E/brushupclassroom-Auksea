cfd5e912eece05adf0202a0a7ee335a600ce1681
Aukse Matulaite <auks0017@edu.easj.dk>
2020-08-26 12:58:05 +0200

    Task 2.

---
+++ ClassRoom/ClassRoom.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    class ClassRoom
+    {
+        public string ClassName;
+        List<string> ClassList = new List<string>();
+        DateTime SemesterStart;
+
+        public ClassRoom() { }
+
+    }
+}
