85ecb047f8a1ae96582bf0e84315fc35c55d7b58
Aukse Matulaite <auks0017@edu.easj.dk>
2020-08-26 12:50:04 +0200

    Task 1

---
+++ ClassRoom/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    class Student
+    {
+        protected string Name { get; set; }
+        protected int BirthMonth { get; set; }
+        protected int Birthday { get; set; }
+
+        public Student(string name, int birthMont, int birthday)
+        {
+            Name = name;
+            BirthMonth = birthMont;
+            Birthday = birthday;
+        }
+
+
+
+
+
+    }
+}
